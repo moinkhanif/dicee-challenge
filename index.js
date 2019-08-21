e5f134e88c6f8d2aea98be7ad4c7297fc1bca07f
Moin Khan <kmoin786@gmail.com>
2019-08-21 23:34:54 +0530

    randomized both dice

---
+++ index.js
@@ -0,0 +1,4 @@
+let randomNumber1 = Math.round(Math.random()*5)+1;
+document.querySelector(".img1").src = `./images/dice${randomNumber1}.png`;
+let randomNumber2 = Math.round(Math.random()*5)+1;
+document.querySelector(".img2").src = `./images/dice${randomNumber2}.png`;
